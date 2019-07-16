1499e4af1bbf8bd5be3c936bb56940ada22c04eb
Pablo <pablo.albendea@outlook.com>
2019-07-16 18:39:41 +0200

    added form functionality and new show page
    - bodyParser installed
    - request installed
    - POST request route added
    - Openweather API call added
    - now form returns result

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const express = require('express');
-const app = express()
+var   express        = require('express'),
+      app            = express(),
+      bodyParser     = require('body-parser'),
+      request        = require('request');
+
+var apiKey = 'f67382942a8313a2d571adf0cf0fde7d';
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
 
+//INDEX ROUTES
 app.get('/', function (req, res) {
 	res.render('index');
+});
+
+app.post('/', function (req, res) {
+	let city = req.body.city;
+	let url =`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+	request(url, function (err, response, body) {
+	    if(err){
+	      	res.render('show', {weather: null, error: 'Error, please try again'});
+	    } else {
+	      	let weather = JSON.parse(body);
+	      	if(weather.main == undefined){
+	        	res.render('show', {weather: null, error: 'Error, please try again'});
+	    	} else {
+	        	let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+	        	res.render('show', {weather: weatherText, error: null});
+	    	}
+	    }
+	});
 });
 
 
